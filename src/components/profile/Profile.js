60a10734b05df805662b456a3dab02d30445c836
Serhii Lytvynenko <mr.litvinenko@gmail.com>
2023-04-20 14:41:19 +0300

    Update Profile.js

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -21,15 +21,15 @@ const {username, tag, location, avatar = defaultImage, stats} = user
         
               <ul className="stats">
                 <li>
-                  <span className="label">Followers</span>
+                  <span className="label">Followers: </span>
                   <span className="quantity">{user.stats.followers}</span>
                 </li>
                 <li>
-                  <span className="label">Views</span>
+                  <span className="label">Views: </span>
                   <span className="quantity">{user.stats.views}</span>
                 </li>
                 <li>
-                  <span className="label">Likes</span>
+                  <span className="label">Likes: </span>
                   <span className="quantity">{user.stats.likes}</span>
                 </li>
               </ul>
