37e5a377d2c51b9c0dafef33320cf9b34a78f911
Serhii Lytvynenko <mr.litvinenko@gmail.com>
2023-04-20 12:35:13 +0300

    add browser router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+<BrowserRouter basename="/goit-react-hw-01-components">
+  <App />
+</BrowserRouter>
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
