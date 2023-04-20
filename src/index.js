896f513e986dd033b449e658a4fa5acb03e6df2f
Serhii Lytvynenko <mr.litvinenko@gmail.com>
2023-04-20 12:38:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
-<BrowserRouter basename="/goit-react-hw-01-components">
-  <App />
-</BrowserRouter>
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
