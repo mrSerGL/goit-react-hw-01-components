2c4ad68655c42406f415efaa1d2a2e79536e6992
Serhii Lytvynenko <mr.litvinenko@gmail.com>
2023-04-23 17:40:32 +0300

    coprr

--- src/components/transactions/TransactionHistory.js
+++ src/components/transactions/TransactionHistory.js
@@ -3,7 +3,7 @@ import css from'./TransactionHistory.module.css';
 
 export default function TransactionHistory({ transactions }) {
   return (
-    <table className="transaction-history">
+    <table className={css.transactionHistory}>
       <thead>
         <tr>
           <th>Type</th>
